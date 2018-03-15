c1942dc8d641df94246ec8415cdb2913caf53f8c
Richard Russon <rich@flatcap.org>
2018-03-15 19:25:10 +0000

    update man pages

--- _man/_generate.sh
+++ _man/_generate.sh
@@ -4,32 +4,36 @@ SRC=${1:-~/neomutt}
 
 [ -d "$SRC" ] || exit 1
 
-cp $(find "$SRC" -name \*.man) .
+for i in "$SRC/doc/"*.man "$SRC/doc/"*.[15]; do
+	MAN="${i##*/}"
+	MAN="${MAN%.*}.man"
+	cp "$i" "$MAN"
+done
 
 for i in *.man; do
-	doclifter $i
+	doclifter "$i"
 done
 
 cp "$SRC/doc/chunk.xsl" .
-cp "$SRC/doc/mutt.xsl" .
-cp "$SRC/doc/mutt.css" .
+cp "$SRC/doc/neomutt.xsl" .
+cp "$SRC/doc/neomutt.css" .
 
 for i in *.man.xml; do
-	xsltproc --param root.filename $i.html --nonet chunk.xsl $i
+	xsltproc --param root.filename "$i.html" --nonet chunk.xsl "$i"
 	mv "${i%.man.xml}man.html" "${i%.man.xml}.html"
 done
 
 for i in *.html; do
-	tidy -q -xml $i
+	tidy -q -xml "$i"
 done
 
 for i in *.html; do
-	sed -i '1,3d' $i
-	sed -i 's/.*<html [^>]\+>/<html>/' $i
-	xmlstarlet sel -t -c "//div[@class='refentry']" $i \
+	sed -i '1,3d' "$i"
+	sed -i 's/.*<html [^>]\+>/<html>/' "$i"
+	xmlstarlet sel -t -c "//div[@class='refentry']" "$i" \
 		| tidy -q -xml \
 		| sed 's/<a id=\([^ ]\+\) \/>/<a class="offset" id=\1><\/a>/g' \
-		| sponge $i
+		| sponge "$i"
 done
 
 echo "Wrapping:" 
@@ -39,7 +43,7 @@ for FILE in *.html; do
 	DESC=${TEXT#* }
 	DESC=${DESC#* }
 
-	echo -e "\t$FILE"
+	echo -e "\\t$FILE"
 	(
 		echo "---"
 		echo "layout: concertina"
@@ -47,13 +51,13 @@ for FILE in *.html; do
 		echo "description: $DESC"
 		echo "---"
 		echo "{% raw %}"
-		cat $FILE
+		cat "$FILE"
 		echo "{% endraw %}"
-	) | sponge $FILE
+	) | sponge "$FILE"
 done
 
-rm -f *.man
-rm -f *.xml
-rm -f *.xsl
-rm -f *.css
+rm -f ./*.man
+rm -f ./*.xml
+rm -f ./*.xsl
+rm -f ./*.css
 
