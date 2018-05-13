66f4c2bdae9595ba260fe7b9b0fb013c55bf8108
toogley <toogley@mailbox.org>
2018-05-13 12:45:52 +0200

    update

--- .test.sh
+++ .test.sh
@@ -6,8 +6,6 @@ http_status=({100..399});
 printf -v all '%s,' ${http_status[@]};
 
 #maybe https://unix.stackexchange.com/a/25378 is better?
-files_to_proof="./_site/**/*.{html,md}"
 
-echo $files_to_proof
-htmlproofer --http-status-ignore $all --assume-extension $files_to_proof
+htmlproofer --http-status-ignore $all --assume-extension ./_site
 
