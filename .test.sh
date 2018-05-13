7de04c841a6943c42538256725611519f998e089
toogley <toogley@mailbox.org>
2018-05-13 12:18:25 +0200

    update

--- .test.sh
+++ .test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-unbuffer jekyll build
+jekyll build
 http_status=({100..399});
 printf -v all '%s,' ${http_status[@]};
 
-unbuffer \ #maybe https://unix.stackexchange.com/a/25378 is better?
- htmlproofer \ 
- --http-status-ignore $all \
- --assume-extension \
- ./_site/**/*.{html,md}
+#maybe https://unix.stackexchange.com/a/25378 is better?
+htmlproofer --http-status-ignore $all --assume-extension ./_site/**/*.{html,md}
 
