1958a80d26f1115ae423f3e152b4adfa2686accc
toogley <toogley@mailbox.org>
2018-05-13 17:27:51 +0200

    update

--- .test.sh
+++ .test.sh
@@ -4,7 +4,12 @@ travis_fold start "generating HTML out of markdown using jekyll"
 jekyll build
 travis_fold end "generating HTML out of markdown using jekyll"
 
+# generates comma separated HTTP error codes
+http_status_to_ignore=({100..399});
+printf -v all '%s,' ${http_status_to_ignore[@]};
+
 function run_link_checker() {
+    echo $1
     travis_fold start "Link checking $1"
 
     htmlproofer \
@@ -15,10 +20,6 @@ function run_link_checker() {
     travis_fold end "Link checking $1"
 }
 export -f run_link_checker
-
-# generates comma separated HTTP error codes
-http_status_to_ignore=({100..399});
-printf -v all '%s,' ${http_status_to_ignore[@]};
 
 find ./_site -type f -iname '*.html' | xargs -n 10 -I {} \
     bash -c 'run_link_checker {}'
