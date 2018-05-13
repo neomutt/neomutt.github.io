210582b8912a808ef56cd394b03310c05869ff79
toogley <toogley@mailbox.org>
2018-05-13 12:34:12 +0200

    update

--- .test.sh
+++ .test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+
 jekyll build
 http_status=({100..399});
 printf -v all '%s,' ${http_status[@]};
 
 #maybe https://unix.stackexchange.com/a/25378 is better?
-htmlproofer --http-status-ignore $all --assume-extension ./_site/**/*.{html,md}
+files_to_proof="./_site/**/*.{html,md}"
+
+echo $files_to_proof
+htmlproofer --http-status-ignore $all --assume-extension $files_to_proof
 
